45473214c182171c1c348f12a6137ed32d21b21e
Nasib <147787755+Nasibyun@users.noreply.github.com>
2026-07-01 23:41:46 +0530

    Hash-table

---
+++ Longest Repeating Character Replacement 424.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+
+        int l = 0,cnt = 0,ans = 0;
+
+        for (int r = 0; r < s.size(); r++) {
+            freq[s[r] - 'A']++;
+            cnt = max(cnt, freq[s[r] - 'A']);
+
+            while ((r - l + 1) - cnt > k) {
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+};
