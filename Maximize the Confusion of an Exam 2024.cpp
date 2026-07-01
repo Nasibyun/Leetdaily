dc25508f8832424069a2569b3fdf8920d4cc5eb7
Nasib <147787755+Nasibyun@users.noreply.github.com>
2026-07-01 23:40:32 +0530

    Hash-table

--- Maximize the Confusion of an Exam 2024.cpp
+++ Maximize the Confusion of an Exam 2024.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int helper(string &s, int k, char target) {
-        int l = 0, cnt=0, ans=0;
+    int maxConsecutiveAnswers(string s, int k) {
+        vector<int> freq(26, 0);
+
+        int l = 0,cnt = 0,ans = 0;
 
         for (int r = 0; r < s.size(); r++) {
-            if (s[r] != target)
-                cnt++;
+            freq[s[r] - 'A']++;
+            cnt = max(cnt, freq[s[r] - 'A']);
 
-            while (cnt > k) {
-                if (s[l] != target)
-                    cnt--;
+            while ((r - l + 1) - cnt > k) {
+                freq[s[l] - 'A']--;
                 l++;
             }
             ans = max(ans, r - l + 1);
         }
         return ans;
-    }
-
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        return max(helper(answerKey, k, 'T'),helper(answerKey, k, 'F'));
     }
 };
