74237b0c6095a46306bb4e2b0ad774f747c2afdd
Nasib <147787755+Nasibyun@users.noreply.github.com>
2026-08-02 21:40:33 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        int sum1 = 0, sum2=0;
+
+        int i = 0, j = n-1;
+
+        while(i<j){
+            sum1 += max(piles[i],piles[j]);
+            sum2 += min(piles[i],piles[j]);
+            i++,j--;
+        }
+        return (sum1 > sum2);
     }
 };
