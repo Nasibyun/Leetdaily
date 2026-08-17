544824757894b8515e5eed8408220a60bb5fd114
Nasib <147787755+Nasibyun@users.noreply.github.com>
2026-08-18 05:00:09 +0530

    Time: 980 ms (12.45%), Space: 27.7 MB (33.87%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -1,49 +1,25 @@
 class Solution {
 public:
-    int stoneGameV(vector<int>& stoneValue) {
-        int n = stoneValue.size();
+    int stoneGameV(vector<int>& a) {
+        int n = a.size();
+        vector<int> pre(n + 1);
+        for (int i = 0; i < n; i++) pre[i + 1] = pre[i] + a[i];
 
-        // Prefix sum
-        vector<int> prefix(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + stoneValue[i];
-        }
+        vector<vector<int>> dp(n, vector<int>(n));
 
-        // dp[l][r] = maximum score for subarray [l...r]
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-
-        // Length of interval
         for (int len = 2; len <= n; len++) {
             for (int l = 0; l + len <= n; l++) {
                 int r = l + len - 1;
-
-                // Try every split
                 for (int k = l; k < r; k++) {
-                    int leftSum = prefix[k + 1] - prefix[l];
-                    int rightSum = prefix[r + 1] - prefix[k + 1];
+                    int x = pre[k + 1] - pre[l];
+                    int y = pre[r + 1] - pre[k + 1];
 
-                    if (leftSum < rightSum) {
-                        dp[l][r] = max(
-                            dp[l][r],
-                            leftSum + dp[l][k]
-                        );
-                    }
-                    else if (leftSum > rightSum) {
-                        dp[l][r] = max(
-                            dp[l][r],
-                            rightSum + dp[k + 1][r]
-                        );
-                    }
-                    else {
-                        dp[l][r] = max(
-                            dp[l][r],
-                            leftSum + max(dp[l][k], dp[k + 1][r])
-                        );
-                    }
+                    if (x < y) dp[l][r] = max(dp[l][r], x + dp[l][k]);
+                    else if (x > y) dp[l][r] = max(dp[l][r], y + dp[k + 1][r]);
+                    else dp[l][r] = max(dp[l][r], x + max(dp[l][k], dp[k + 1][r]));
                 }
             }
         }
-
         return dp[0][n - 1];
     }
 };
