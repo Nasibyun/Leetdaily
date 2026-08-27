eef501fa462cc8fd7da7e690b37011ce9d8e81d8
Nasib <147787755+Nasibyun@users.noreply.github.com>
2026-08-28 00:18:32 +0530

    Time: 3 ms (69.43%), Space: 9.7 MB (90.45%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,37 +1,52 @@
 class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
-        int cnt[26] = {};
-
-        for (char c : s)
+        int n = s.length();
+        vector<int> cnt(26, 0);
+        for (char c : s) {
             cnt[c - 'a']++;
-
-        string ans = "";
-
-        for (int i = 0; i < target.size(); i++) {
-            for (int c = target[i] - 'a' + 1; c < 26; c++) {
-
-                if (cnt[c] == 0)
-                    continue;
-
-                string cur = target.substr(0, i);
-                cur += char('a' + c);
-
-                cnt[c]--;
-                for (int j = 0; j < 26; j++) {
-                    cur += string(cnt[j], char('a' + j));
-                }
-                if (ans == "" || cur < ans)
-                    ans = cur;
-                cnt[c]++;
-            }
-            int x = target[i] - 'a';
-
-            if (cnt[x] == 0)
-                break;
-            cnt[x]--;
         }
+        string res = string(n, ' ');
 
-        return ans;
+        int i = 0;
+        while (i < n) {
+            char t_char = target[i];
+            if (cnt[t_char - 'a'] > 0) {
+                res[i] = t_char;
+                cnt[t_char - 'a']--;
+                i++;
+            } else {
+                break;
+            }
+        }
+        int limit = (i == n) ? n - 1 : i;
+        for (int curr = limit; curr >= 0; curr--) {
+            if (res[curr] != ' ') {
+                cnt[res[curr] - 'a']++;
+                res[curr] = ' ';
+            }
+            char target_char = target[curr];
+            int choice = -1;
+            for (int c = (target_char - 'a') + 1; c < 26; c++) {
+                if (cnt[c] > 0) {
+                    choice = c;
+                    break;
+                }
+            }
+            if (choice != -1) {
+                res[curr] = (char)('a' + choice);
+                cnt[choice]--;
+                int write_idx = curr + 1;
+                for (int c = 0; c < 26; c++) {
+                    while (cnt[c] > 0) {
+
+                        res[write_idx++] = (char)('a' + c);
+                        cnt[c]--;
+                    }
+                }
+                return res;
+            }
+        }
+        return "";
     }
 };
