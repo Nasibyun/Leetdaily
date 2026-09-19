2c3528ea81ed96374dd7c695643e7d7ad9c50e20
Nasib <147787755+Nasibyun@users.noreply.github.com>
2026-09-20 02:45:37 +0530

    Time: 0 ms (100%), Space: 18.5 MB (16.51%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     void dfs(int row, int col, vector<vector<int>>& ans, vector<vector<int>>& image,
-        int newColor, int delrow[], int delcol[], int inicolor){
+        int newColor, int inicolor){
 
         ans[row][col] = newColor;
         int n = image.size();
         int m = image[0].size();
+
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
 
         for(int i=0; i<4; i++){
             int nr = row + delrow[i];
@@ -13,16 +16,14 @@ public:
 
             if(nr >= 0 && nr < n && nc >= 0 && nc < m && image[nr][nc] == inicolor 
                 && ans[nr][nc] != newColor){
-                dfs(nr,nc,ans,image,newColor, delrow,delcol,inicolor);
+                dfs(nr,nc,ans,image,newColor,inicolor);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int inicolor = image[sr][sc];
         vector<vector<int>> ans = image;
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
-        dfs(sr,sc,ans,image,color,delrow,delcol,inicolor);
+        dfs(sr,sc,ans,image,color,inicolor);
         return ans;
     }
 };
